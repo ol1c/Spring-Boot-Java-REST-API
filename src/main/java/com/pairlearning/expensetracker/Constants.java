e0f073d17e37344f4f6cf838b428305e4f0013fa
ol1c <155087424+ol1c@users.noreply.github.com>
2025-02-23 20:34:35 +0100

    JSON token validation

---
+++ src/main/java/com/pairlearning/expensetracker/Constants.java
@@ -0,0 +1,8 @@
+package com.pairlearning.expensetracker;
+
+public class Constants {
+
+    public static final String API_SECRET_KEY = "ProjectExpenseTrackerApiSecretKeyForJsonTokenGeneration!";
+
+    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
+}
--- src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
@@ -1,7 +1,12 @@
 package com.pairlearning.expensetracker;
 
+import com.pairlearning.expensetracker.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.logging.Filter;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
@@ -10,4 +15,13 @@ public class ExpenseTrackerApiApplication {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
 	}
 
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+	}
 }
