839c6086330a2a7c49774ab7ffee7ced1d39de1f
ol1c <155087424+ol1c@users.noreply.github.com>
2025-02-24 11:41:06 +0100

    Adding category

--- src/main/java/com/pairlearning/expensetracker/resources/CategoryResource.java
+++ src/main/java/com/pairlearning/expensetracker/resources/CategoryResource.java
@@ -1,17 +1,34 @@
 package com.pairlearning.expensetracker.resources;
 
+import com.pairlearning.expensetracker.domain.Category;
+import com.pairlearning.expensetracker.services.CategoryService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryResource {
 
+    @Autowired
+    CategoryService categoryService;
+
     @GetMapping("")
     public String getAllCategories(HttpServletRequest request) {
         int userId = (Integer) request.getAttribute("userId");
         return "Authenticated! UserId: " + userId;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Category> addCategory(HttpServletRequest request, @RequestBody Map<String, Object> categoryMap) {
+        int userId = (Integer) request.getAttribute("userId");
+        String title = (String) categoryMap.get("title");
+        String description = (String) categoryMap.get("description");
+        Category category = categoryService.addCategory(userId, title, description);
+        return  new ResponseEntity<>(category, HttpStatus.CREATED);
     }
 }
