dde9e4c02e7e95cf4468a0dff9fe190ab611caf7
ol1c <155087424+ol1c@users.noreply.github.com>
2025-02-25 14:48:08 +0100

    Connect with webpage

--- src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/pairlearning/expensetracker/ExpenseTrackerApiApplication.java
@@ -1,10 +1,13 @@
 package com.pairlearning.expensetracker;
 
 import com.pairlearning.expensetracker.filters.AuthFilter;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.logging.Filter;
 
@@ -23,5 +26,18 @@ public class ExpenseTrackerApiApplication {
 		registrationBean.setFilter(authFilter);
 		registrationBean.addUrlPatterns("/api/categories/*");
 		return registrationBean;
+	}
+
+	@Bean
+	public  FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		registrationBean.setFilter(new CorsFilter(source));
+		registrationBean.setOrder(0);
+		return  registrationBean;
 	}
 }
